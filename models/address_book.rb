8a1c2ad79df3d1bfd17be43951cb18bc709e9be2
Ross Ginn <ross.ginn@gmail.com>
2017-09-27 19:11:28 -0600

    7 assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -43,4 +43,40 @@ end
 # #11
   entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def interactive_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+
+    return nil
+  end
+
+  # Search AddressBook for a specific entry by name
+     def binary_search(name)
+       lower = 0
+     upper = entries.length - 1
+
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     # #5
+     return nil
+     end
+
 end
